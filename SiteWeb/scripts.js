f3f8dc3b02901ec923f7e946900c8fb1d423a46d
Penzanto <124901698+Penzanto@users.noreply.github.com>
2023-04-11 16:55:49 +0200

    upload des fichiers de fin de sceance
    amélioration de la page web

--- SiteWeb/scripts.js
+++ SiteWeb/scripts.js
@@ -1,12 +1,7 @@
 window.onload=function() {
-    var VarTest = "123";
-    var SHT31_Temp = VarTest;
-    var SHT31_Humid = VarTest;
-    var DHT11_Temp = VarTest;
-    var DHT11_Humid = VarTest;
-    var cap_Dist = VarTest;
-    var cap_Masse = VarTest;
+    document.getElementById("SHT31_Temp").innerHTML = 999;
 
     //document.getElementById('text').innerHTML = text;
 }
+
 

1743eadb5529b828108e0682a02110f90e79b6fe
Penzanto <124901698+Penzanto@users.noreply.github.com>
2023-04-30 22:45:41 +0200

    ajout de du framework fastify/static au ServerWeb & première version du serverweb fonctionnelle

---
+++ ServerWeb/SiteWeb/clientScripts.js
@@ -0,0 +1,23 @@
+window.onload=function() {
+    var VarTest = "123";
+    var SHT31_Temp = VarTest;
+    var SHT31_Humid = VarTest;
+    var DHT11_Temp = VarTest;
+    var DHT11_Humid = VarTest;
+    var cap_Dist = VarTest;
+    var cap_Masse = VarTest;
+
+    //lien des variables avec ce qui est affiche sur la page html
+    document.getElementById('SHT31_Temp').innerHTML = SHT31_Temp;
+    document.getElementById('SHT31_Humid').innerHTML = SHT31_Humid;
+    //
+    document.getElementById('DHT11_Temp').innerHTML = DHT11_Temp;
+    document.getElementById('DHT11_Humid').innerHTML = DHT11_Humid;
+    //
+    document.getElementById('Distance').innerHTML = cap_Dist;
+    //
+    document.getElementById('Masse').innerHTML = cap_Masse;
+    
+}
+
+//console.log("je suis un programeur")
